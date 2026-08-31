8138dfe080a6e399d801a64f5d6fb1427a160329
M4ff30 <127723981+real-Maffeo@users.noreply.github.com>
2026-08-31 23:05:43 +0200

    Delete js/script.js
    Creato al suo posto registrazione.js e login.js

--- js/script.js
+++
@@ -1,6 +0,0 @@
-// js/script.js
-const errore = new URLSearchParams(window.location.search).get("errore");
-if (errore) {
-    const div = document.getElementById("errore");
-    if (div) div.textContent = errore;
-}
