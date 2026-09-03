8c4b09771a1cbfd2ba4624195bc17b25657ce942
M4ff30 <127723981+real-Maffeo@users.noreply.github.com>
2026-09-03 23:45:18 +0200

    Delete js/registrazione.js

--- js/registrazione.js
+++
@@ -1,5 +0,0 @@
-const errore = new URLSearchParams(window.location.search).get("errore");
-if (errore) {
-    const div = document.getElementById("errore");
-    if (div) div.textContent = errore;
-}
